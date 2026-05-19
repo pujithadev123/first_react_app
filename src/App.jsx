23291bb369a3ddbfc67b900262ea0509f0c74932
pujithadev123 <sivapujitha3012@gmail.com>
2026-05-19 11:04:49 +0530

    all files are saved

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+function App(){
+  return(
+    <>
+    <h1>This is first react deploy app</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt quas mollitia voluptates magni cupiditate temporibus esse adipisci laboriosam qui aspernatur, fugit sit blanditiis ea, eveniet assumenda est. Atque, aliquam harum.</p>
+  <details>
+    
+    <summary>
+      React
+     
+    </summary>
+
+     <p>React is a js library used to build dynamic and interactive websites without refresh the page.</p>
+  </details>
+
+    </>
+  )
+}
+export default App
